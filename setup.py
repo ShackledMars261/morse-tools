4d0cbd9dc4afeb89b41fa7b56c2e7ea216c080bc
Jack Martin <jrm9229@gmail.com>
2024-10-08 09:29:09 -0500

    add -v command

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="morse-tools",
     py_modules=["wav2morse"],
     scripts=['bin/morse-tools'],
-    version="1.0",
+    version="1.0.1",
     description="Various tools for working with morse code",
     author="ShackledMars261",
 )
